a1a69b66ce615f4646e7a9f2d840de2f7c34647d
Dâmaris Dwayne <damarisdwayne@outlook.com>
2021-03-27 09:06:12 -0300

    Acrescentei novamnete a frase

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -4,6 +4,8 @@ namespace Teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello Wooooorld!");
+            Console.WriteLine("bom dia");
+            Console.WriteLine("O ceu é azul não");
         }
     }
 }
